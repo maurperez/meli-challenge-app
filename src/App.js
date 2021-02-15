80665471742e07024fe694053e12d7a1c101ae6a
Agustin Veloso <mauro.alejandro.perez@outlook.es>
2021-02-15 13:43:22 -0300

    setea el navbar como un componente global

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { CajaDeBusqueda } from './pages/CajaDeBusqueda'
 import { ResultadosDeBusqueda } from './pages/ResultadosDeBusqueda'
 import { Container } from './components/Container'
 import './styles/index.scss'
+import { NavBar } from './components/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
       <Container>
+        <NavBar/>
         <Switch>
           <Route path='/' exact>
             <CajaDeBusqueda />
           </Route>
-          <Route path='/items'>
+          <Route path='/items' exact>
             <ResultadosDeBusqueda></ResultadosDeBusqueda>
           </Route>
         </Switch>
