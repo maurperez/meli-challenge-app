66acb4be1ff7a2016ffeecac646391b3d97ed6e9
Agustin Veloso <mauro.alejandro.perez@outlook.es>
2021-02-16 23:10:43 -0300

    format code

--- src/adapters/product.js
+++ src/adapters/product.js
@@ -2,7 +2,9 @@ const BASE_URL = process.env.REACT_APP_API_URL
 
 const search = async (query, limit) => {
   console.log(BASE_URL)
-  const response = await fetch(`${BASE_URL}/items?q=${query}&limit=${limit || 50}`)
+  const response = await fetch(
+    `${BASE_URL}/items?q=${query}&limit=${limit || 50}`
+  )
   if (!response.ok) {
     throw new Error('API Error')
   }
